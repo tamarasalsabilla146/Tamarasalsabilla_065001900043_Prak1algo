804724ee60707722ca3a0df3e55d0171a8002966
tamarasalsabill <80630335+tamarasalsabilla146@users.noreply.github.com>
2021-09-22 13:09:18 +0700

    Prak_Algo_Tamarasalsabillajm_065001900043

---
+++ Prak1_TamaraSalsabilla_065001900043.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 22 08:28:58 2021
+
+@author: tamara
+"""
+ 
+print("        @                @@@@@              @       ")
+print("       @ @               @   @             @ @      ")
+print("      @   @              @  @             @   @     ")
+print("     @     @             @ @             @     @    ")
+print("    @       @            @@             @       @   ")
+print("   @@@@@@@@@@@           @ @           @@@@@@@@@@@  ")
+print("  @           @          @  @         @           @ ")
+print(" @             @         @   @       @            @ ")
+print(" @               @        @   @      @            @ ")
+
+print('Dia berkata, "Tolong kerjakan laporan praktikum"')
+print('I`m eating right now')
